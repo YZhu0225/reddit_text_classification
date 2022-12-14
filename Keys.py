420d31307203b39482c243b0147bbea23333aea7
XquanL <112578003+XquanL@users.noreply.github.com>
2022-12-14 16:21:06 -0500

    Delete Keys.py

--- Keys.py
+++
@@ -1,5 +0,0 @@
-reddit_keys = {'client_id' : "Q1w42RHhLq2fgwljAk_k-Q",		 # your client id
-'client_secret': "enUJfFthiZRynGfPQtoK1nCxRer2Dw",	 # your client secret
-'usernme' : "xl395", #profile username
-'password' : "12xiao34quanAria!", #profile password
-'user_agent' :"706_post"}	 # your user agent
